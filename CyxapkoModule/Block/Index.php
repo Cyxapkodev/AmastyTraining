15093b0996068d55714cbda7f92ef7206a9eb13f
cyxapko <cyxapko@yandex.ru>
2023-03-27 21:34:25 +0300

    Ex3.1

--- CyxapkoModule/Block/Index.php
+++ CyxapkoModule/Block/Index.php
@@ -3,16 +3,38 @@
 
 namespace Amasty\CyxapkoModule\Block;
 
-
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Template;
+
 
 
 class Index extends Template
 {
-    public function sayHiTo()
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+    public function __construct(
+        Template\Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = [])
     {
-        return 'Hello world!';
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
     }
 
+    public function getWelcomeText(): string
+    {
+        return $this->scopeConfig->getValue('cyxapko_config/general/welcome_text') ?: '';
+    }
+    public function isQtyHidden()
+    {
+        return  $this->scopeConfig->getValue('cyxapko_config/general/hide_qty');
+    }
+
+    public function getViewQtyValue()
+    {
+        return  $this->scopeConfig->getValue('cyxapko_config/general/view_qty');
+    }
 
 }
--- CyxapkoModule/Controller/Index/index.php
+++ CyxapkoModule/Controller/Index/index.php
@@ -3,8 +3,9 @@
 namespace Amasty\CyxapkoModule\Controller\Index;
 
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Controller\ResultFactory;
-//use tests\unit\Magento\FunctionalTestFramework\Util\Path\UrlFormatterTest;
+
 
 class Index implements ActionInterface
 {
@@ -13,14 +14,26 @@ class Index implements ActionInterface
      */
     private $resultFactory;
 
-    public function __construct(ResultFactory $resultFactory)
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        ResultFactory $resultFactory,
+        ScopeConfigInterface $scopeConfig)
     {
         $this->resultFactory = $resultFactory;
+        $this->scopeConfig = $scopeConfig;
     }
+
     public function execute()
     {
+        if ($this->scopeConfig->isSetFlag('cyxapko_config/general/enabled')) {
+            return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        } else {
+            die('Sorry, honey go home!');
+        }
 
-       return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
-
 }
